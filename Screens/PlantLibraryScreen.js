dc606f4c8fab964c8be5af27c1d2e9a1d004ba69
lbeliazi2 <lenabel2000@hotmail.fr>
2022-04-19 21:21:26 +0200

    library front

--- Screens/PlantLibraryScreen.js
+++ Screens/PlantLibraryScreen.js
@@ -1,6 +1,6 @@
 // Screens/PlantLibraryScreen.js
 import React from 'react'
-import {ScrollView, StyleSheet, View,Text} from 'react-native'
+import {ScrollView,FlatList, Image, StyleSheet, View,Text} from 'react-native'
 
 
 
@@ -9,6 +9,52 @@ class PlantLibraryScreen extends React.Component{
         return(
         <ScrollView>
               <Text style={styles.menu}>Plant Library</Text>
+              <Text style={styles.title}>This is your plant library: here will appear all of the monitored plants that you have in your possession.</Text>
+              <Text style={styles.title}>Recently added plants: </Text>
+                <FlatList
+                  horizontal={true}
+                  showsHorizontalScrollIndicator={true}
+                  // remplir data avec les données des plantes
+                  data={[
+                     {key: require('../assets/plante.jpg')}
+                  ]}
+                  renderItem={ ({ item, index }) => (
+                    <Image source={item.key} /* Use item to set the image source */
+                      key={index} /* Important to set a key for list items,
+                                     but it's wrong to use indexes as keys, see below */
+                      style={{
+                        width: 200,
+                        height:150,
+                        borderWidth:2,
+                        borderColor:'#8fbc8f',
+                        margin:8
+                      }}
+                    />
+                  )}
+                />
+                <Text style={styles.title}>All of your plants: </Text>
+                <FlatList
+                     horizontal={true}
+                     showsHorizontalScrollIndicator={true}
+                     // remplir data avec les données des plantes
+                     data={[
+                         {key: require('../assets/fleur.jpg')},
+                         {key: require('../assets/plante.jpg')}
+                     ]}
+                     renderItem={ ({ item, index }) => (
+                         <Image source={item.key} /* Use item to set the image source */
+                         key={index} /* Important to set a key for list items,
+                                                     but it's wrong to use indexes as keys, see below */
+                         style={{
+                            width:200,
+                            height:150,
+                            borderWidth:2,
+                            borderColor:'#8fbc8f',
+                            margin:8
+                         }}
+                     />
+                   )}
+                />
         </ScrollView>
         )
     }
