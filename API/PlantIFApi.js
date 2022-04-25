c1ef59919bebc98088193e054b33baf99c3793b1
lbeliazi2 <lenabel2000@hotmail.fr>
2022-04-25 17:05:45 +0200

    fonctions API

--- API/PlantIFApi.js
+++ API/PlantIFApi.js
@@ -24,3 +24,84 @@ export function addPlant(plantInfos){
     }
     return res;
 }
+
+export function getPlantLibrary(){
+    console.log("Get the plant library")
+
+    // const url="http://monServeur.com/getPlantLibrary"
+    // return fetch(
+    //     url,
+    //     {
+    //         method: 'get',
+    //         headers: {
+    //             'Content-Type':'application/json',
+    //         },
+    //     }
+    // )
+    // .then((response)=>response.json())
+    // .catch((error)=>console.error(error))
+
+
+    var res = Math.random();
+    if (res <= 0.5){
+        res=0
+    }else{
+        res= 1
+    }
+    return res;
+}
+
+export function getPlantInfos(plantId){
+    console.log("Get plant infos")
+
+    // const url="http://monServeur.com/getPlantInfos"
+    // return fetch(
+    //     url,
+    //     {
+    //         method: 'post',
+    //         body: plantId,
+    //         headers: {
+    //             'Content-Type':'application/json',
+    //         },
+    //     }
+    // )
+    // .then((response)=>response.json())
+    // .catch((error)=>console.error(error))
+
+
+    var res = Math.random();
+    if (res <= 0.5){
+        res=0
+    }else{
+        res= 1
+    }
+    return res;
+}
+
+export function modifyPlantInfos(plantInfos){
+    console.log("Modify plant infos")
+
+    // const url="http://monServeur.com/getPlantInfos"
+    // return fetch(
+    //     url,
+    //     {
+    //         method: 'post',
+    //         body: plantInfos,
+    //         headers: {
+    //             'Content-Type':'application/json',
+    //         },
+    //     }
+    // )
+    // .then((response)=>response.json())
+    // .catch((error)=>console.error(error))
+
+
+    var res = Math.random();
+    if (res <= 0.5){
+        res=0
+    }else{
+        res= 1
+    }
+    return res;
+}
+
