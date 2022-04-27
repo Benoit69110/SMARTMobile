0a1e05b930bbdb83b6860c8cb7ada5c7c78ac6ce
lbeliazi2 <lenabel2000@hotmail.fr>
2022-04-27 13:47:24 +0200

    fix network issues

--- API/PlantIFApi.js
+++ API/PlantIFApi.js
@@ -1,26 +1,28 @@
 export function addPlant(plantInfos){
     console.log("Add a plant with API")
 
-    // const url="http://monServeur.com/addPlant"
-    // return fetch(
-    //     url,
-    //     {
-    //         method: 'post',
-    //         body: plantInfos,
-    //         headers: {
-    //             'Content-Type':'application/json',
-    //         },
-    //     }
-    // )
-    // .then((response)=>response.json())
-    // .catch((error)=>console.error(error))
+     const url="http://192.168.020/ActionServlet"
+     return fetch(
+         url,
+         {
+            method: 'post',
+            data: {
+               todo: "getPlant"
+            }
+             headers: {
+                 'Content-Type':'application/json',
+             },
+         }
+     )
+     .then((response)=>response.json())
+     .catch((error)=>console.error(error))
 
 
-    var res = Math.random();
+    /*var res = Math.random();
     if (res <= 0.5){
         res=0
     }else{
         res= 1
     }
-    return res;
+    return res;*/
 }
