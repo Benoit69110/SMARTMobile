5e55fdd2b6b1e5ce44edc8d82a65f1a2a3fc3bcf
lbeliazi2 <lenabel2000@hotmail.fr>
2022-05-03 15:24:51 +0200

    map liste

--- Screens/SocialNetworkScreen.js
+++ Screens/SocialNetworkScreen.js
@@ -10,20 +10,52 @@ const LOGO = Image.resolveAssetSource(LogoImage).uri;
 
 class SettingsScreen extends React.Component{
 
+
      state = {
         region: {
             latitude: 45.771944,
             longitude: 4.8901709,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421
+            latitudeDelta: 0,
+            longitudeDelta: 0
         },
         myPlant: {
             latitude: 45.771944,
             longitude: 4.8901709,
-            latituteDelta: 0.01,
-            longitudeDelta: 0.01
-        }
-     }
+            latituteDelta: 0,
+            longitudeDelta: 0
+        },
+      markers: [
+        {
+          key: "Hi",
+          latlng:  {
+               latitude: 45.771944,
+               longitude: 4.86,
+               latituteDelta: 0.01,
+               longitudeDelta: 0.01
+          },
+          title: "Mayarachide",
+          description: "Type de plante: Rafflesia arnoldi"
+        },
+        {
+           key: "Hii",
+           latlng:  {
+               latitude: 45.771944,
+               longitude: 4.93,
+               latituteDelta: 0.01,
+               longitudeDelta: 0.01
+           },
+           title: "Benoit le renoi",
+           description: "Type de plante : Rose rouge"
+        },
+        {
+          key: "Hiii",
+          latlng: {latitude: 52,longitude: 5.00},
+          title: "Plus",
+          description: "Hello"
+        },
+      ],
+     };
+
 
     setRegion = (region) => {
       this.setState({ region : region });
@@ -43,8 +75,18 @@ class SettingsScreen extends React.Component{
                 >
                 <Marker
                     coordinate={this.state.myPlant}
-                    image={{uri:LOGO}}
+                    pinColor = "red"
                 />
+                {this.state.markers.map((marker) => (
+                    <Marker
+                            key={marker.key}
+                            coordinate={marker.latlng}
+                            title={marker.title}
+                            description={marker.description}
+                            pinColor = "green"
+                    />
+
+                ))}
             </MapView>
             <Text style={styles.text}>Current latitude: {this.state.region.latitude}</Text>
             <Text style={styles.text}>Current longitude: {this.state.region.longitude}</Text>
